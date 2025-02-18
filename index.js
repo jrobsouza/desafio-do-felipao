d8c203d433061c0a041a90cfe0b29793718ae063
Jose Roberto <jrobsouza@hotmail.com>
2025-02-18 20:13:32 -0300

    gerando os codigos do desafio do Felipao

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log ("Ola mundo")
+// Variáveis para armazenar o nome do herói e a quantidade de experiência (XP)
+let nomeHeroi = "Arthur";
+let xpHeroi = 7500; // Altere este valor para testar diferentes níveis de XP
+
+// Variável para armazenar o nível do herói
+let nivelHeroi = "";
+
+// Estrutura de decisão para classificar o nível do herói baseado no XP
+if (xpHeroi < 1000) {
+  nivelHeroi = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+  nivelHeroi = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+  nivelHeroi = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+  nivelHeroi = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+  nivelHeroi = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+  nivelHeroi = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+  nivelHeroi = "Imortal";
+} else if (xpHeroi >= 10001) {
+  nivelHeroi = "Radiante";
+}
+
+// Saída: exibir a mensagem com o nome e nível do herói
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivelHeroi}`);
+
